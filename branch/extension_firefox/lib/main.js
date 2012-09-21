d99d098a5f35ce5ddf73aa18d8e8cf195bbeb124
ricky.tan.xin@gmail.com <ricky.tan.xin@gmail.com@a50a8dd7-28e5-0e6a-86bd-19665268a992>
2012-09-21 06:11:35 +0000

    firefox version finished!

--- branch/extension_firefox/lib/main.js
+++ branch/extension_firefox/lib/main.js
@@ -1,9 +1,11 @@
 var pageMod = require("page-mod");
 var self = require("self");
+var url = self.data.url("out.js");
+
 pageMod.PageMod({
   include: [
 	"http://grsinfo.zju.edu.cn/cultivate/menu/cultivate_menu_student.jsp",
 	"http://g.zju.edu.cn/cultivate/menu/cultivate_menu_student.jsp"],
   contentScriptWhen: 'end',
-  contentScriptFile: self.data.url("loader.js")
+  contentScript: "try{var s=document.createElement('script');s.src='"+url+"';document.getElementsByTagName('head')[0].appendChild(s);}catch(e){alert(e)}"
 });
